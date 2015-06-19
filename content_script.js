cbd8af8500db8502eeefe07d89f000acbd6da771
Adam Chalmers <lewaarkhan@gmail.com>
2015-06-20 00:26:05 +1000

    Copied from Snake People.

--- content_script.js
+++ content_script.js
@@ -1,19 +1,21 @@
 walk(document.body);
 
-function walk(node) 
+document.title = replaceText(document.title);
+
+function walk(node)
 {
     // I stole this function from here:
     // http://is.gd/mwZp7E
-    
+
     var child, next;
 
-    switch ( node.nodeType )  
+    switch ( node.nodeType )
     {
         case 1:  // Element
         case 9:  // Document
         case 11: // Document fragment
             child = node.firstChild;
-            while ( child ) 
+            while ( child )
             {
                 next = child.nextSibling;
                 walk(child);
@@ -27,14 +29,38 @@ function walk(node)
     }
 }
 
-function handleText(textNode) 
-{
-    var v = textNode.nodeValue;
+function handleText(textNode) {
+  textNode.nodeValue = replaceText(textNode.nodeValue);
+}
 
+function replaceText(v)
+{
     v = v.replace(/\bBalfour\b/g, "Bal5");
     v = v.replace(/\bbalfour\b/g, "bal5");
-    v = v.replace(/\bbalfies\b/g, "bal3s");
-    v = v.replace(/\bBalfies\b/g, "Bal3s");
+
+    // Extras to enable later on
     
-    textNode.nodeValue = v;
+    /*r = Math.random();
+    if (r < 0.4) {
+        v = v.replace(/\bBalfour\b/g, "Bal4");
+        v = v.replace(/\bbalfour\b/g, "bal4");
+    } else if (r < 0.8) {
+        v = v.replace(/\bBalfour\b/g, "Bal5");
+        v = v.replace(/\bbalfour\b/g, "bal5");
+    } else if (r < 0.9) {
+        v = v.replace(/\bBalfour\b/g, "Balfour2thefloor");
+        v = v.replace(/\bbalfour\b/g, "balfour2thefloor");
+    } else {
+        v = v.replace(/\bBalfour\b/g, "Bal420");
+        v = v.replace(/\bbalfour\b/g, "bal420");
+    }
+    
+    if (r < 0.4) {
+        v = v.replace(/\bBalfies\b/g, "Balthrees");
+        v = v.replace(/\bbalfies\b/g, "balthrees");       
+    } else {
+        v = v.replace(/\bBalfies\b/g, "Bal3z");
+        v = v.replace(/\bbalfies\b/g, "bal3z"); 
+    }
+    return v;*/
 }
